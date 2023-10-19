b253e5f92d8128554a542fd75dbbabd011b8d96e
Kent Miguel <kentmiguel10@gmail.com>
2023-10-19 18:48:15 -0400

    Created navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
+
+export default function Navbar() {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Congress", href: "/" },
+    { label: "Member", href: "/member" },
+    { label: "Bill", href: "/bill" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">Congress Icon </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+}
