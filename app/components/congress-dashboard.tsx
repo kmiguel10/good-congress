9f22ba5a4e3ad83510e2b96a2e94746c47d3fcd7
Kent Miguel <kentmiguel10@gmail.com>
2023-10-20 16:22:50 -0400

    Add nav bar inside dashboard

--- app/components/congress-dashboard.tsx
+++ app/components/congress-dashboard.tsx
@@ -116,6 +116,12 @@ export default async function CongressDashboard() {
 
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <div className="flex items-center space-x-2">
+          <p>All,House,Senate</p>
+        </div>
+      </div>
       <Tabs defaultValue="overview" className="space-y-4">
         <TabsList>
           <TabsTrigger value="overview">Overview</TabsTrigger>
