93c888715309ada650bf34b3aecfa146e33eb55a
Kent Miguel <kentmiguel10@gmail.com>
2024-01-18 18:16:26 -0500

    Update page.tsx
    temporary fix

--- app/legislators/[id]/components/top-contributor-table.tsx/page.tsx
+++ app/legislators/[id]/components/top-contributor-table.tsx/page.tsx
@@ -4,7 +4,7 @@ import { DataTable } from "./data-table";
 interface Props {
   data: FundraisingContributorData[];
 }
-export default async function TopContributorsTable({ data }: Props) {
+export default async function TopContributorsTable({ data }: any) {
   return (
     <div className="container mx-auto py-1">
       <DataTable columns={columns} data={data} />
